cfb078eb869d45524febb12beadcf0a479bf65f0
Erzhan <you@example.com>
2022-04-21 14:55:20 +0600

    asd

--- post/api/serializers.py
+++ post/api/serializers.py
@@ -1,12 +1,9 @@
-from django.conf import settings
-from django.db import models
+from rest_framework import serializers
+from post.models import Post
 
 
-class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    content = models.TextField(null=True, blank=True)
-    image = models.ImageField(default='default.jpg', upload_to='post_image/')
+class StatusSerializer(serializers.ModelSerializer):
 
-    def __str__(self):
-        return self.title
+    class Meta:
+        model = Post
+        fields = ['user', 'title', 'content', 'image']
--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField(null=True, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='post_image/')
+
+    def __str__(self):
+        return self.title
